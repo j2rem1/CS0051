0c0ba71864d81c70e6eab45f1537646594c8d7b3
j2rem1 <134146964+j2rem1@users.noreply.github.com>
2025-01-31 10:26:31 +0800

    act1_fabian_atud.cpp

--- act1_fabian_atud.cpp
+++ act1_fabian_atud.cpp
@@ -23,46 +23,35 @@ void traffic_light_simulation(int starting_road) {
             int i = (starting_road - 1 + o) % road_number + 1;
             
             mtx.lock();
-            
             current_road = i;
             green_light = true;
             yellow_light = false;
             cout << "Road " << current_road << ": Green light ON\n";
-            
             mtx.unlock();
 
             for (int t = 0; t < glight_time; t += incrementor) {
                 this_thread::sleep_for(chrono::seconds(incrementor));
                 mtx.lock();
-                
                 cout << "Road " << current_road << ": Green light ON (" << glight_time - t - incrementor << "s left)\n";
-                
                 mtx.unlock();
             }
 
             mtx.lock();
-            
             green_light = false;
             yellow_light = true;
             cout << "Road " << current_road << ": Yellow light ON\n";
-            
             mtx.unlock();
 
             for (int t = 0; t < ylight_time; t += incrementor) {
                 this_thread::sleep_for(chrono::seconds(incrementor));
-                
                 mtx.lock();
-                
                 cout << "Road " << current_road << ": Yellow light ON (" << ylight_time - t - incrementor << "s left)\n";
-                
                 mtx.unlock();
             }
 
             mtx.lock();
-            
             yellow_light = false;
             cout << "Road " << current_road << ": Red light ON\n";
-            
             mtx.unlock();
             this_thread::sleep_for(chrono::seconds(1)); 
         }
@@ -72,17 +61,23 @@ void traffic_light_simulation(int starting_road) {
 void car_state(int road_id) {
     while (true) {
         mtx.lock();
-        
         if (current_road == road_id) {
             if (green_light) {
-                cout << "Road " << road_id << ": Cars are going through the intersection.\n";
+                if (road_id == 1) {
+                    cout << "Road " << road_id << ": Lane 1 turning right, Lane 2 going straight.\n";
+                } else if (road_id == 2) {
+                    cout << "Road " << road_id << ": Lane 3 turning right, Lane 4 going straight.\n";
+                } else if (road_id == 3) {
+                    cout << "Road " << road_id << ": Lane 5 turning right, Lane 6 going straight.\n";
+                } else if (road_id == 4) {
+                    cout << "Road " << road_id << ": Lane 7 turning right, Lane 8 going straight.\n";
+                }
             } else if (yellow_light) {
                 cout << "Road " << road_id << ": Cars are slowing down.\n";
             } else {
                 cout << "Road " << road_id << ": Cars are stopped.\n";
             }
         }
-        
         mtx.unlock();
         this_thread::sleep_for(chrono::seconds(incrementor));
     }
@@ -90,7 +85,6 @@ void car_state(int road_id) {
 
 int main() {
     int starting_road;
-
 
     while (true) {
         cout << "Enter the starting road (1-4): ";
